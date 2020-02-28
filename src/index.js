bb5472f2264e5b587b908bba9add293598b44eb3
GeoHernandez27 <leopardo2@gmail.com>
2020-02-28 15:34:14 -0700

    created store and reducers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import allReducers from './reducers'
 
+const store = createStore(allReducers);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
