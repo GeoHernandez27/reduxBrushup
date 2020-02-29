76bbfea4c65b973d414ff56cbae1eb1078e6d92c
GeoHernandez27 <leopardo2@gmail.com>
2020-02-28 17:28:41 -0700

    created container for user list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
 import './App.css';
+import UserList from './containers/userList';
+
 
 function App() {
   return (
     <div>
             <h2>User List:</h2>
+            <UserList />
             <hr />
             <h2>User Details:</h2>
     </div>
