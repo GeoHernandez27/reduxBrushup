1f841f79c1597536ae7e765daea2ccf77cac7127
GeoHernandez27 <leopardo2@gmail.com>
2020-03-04 20:25:18 -0700

    created action creator

--- src/containers/userList.js
+++ src/containers/userList.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
+import {selectUser} from '../actions';
 
 
 class UserList extends Component {
     createListItems(){
         return this.props.users.map((user) => {
             return (
-                <li key={user.id}>{user.first} {user.last}</li>
+                <li onClick={() => this.props.selectUser(user)} key={user.id}>{user.first} {user.last}</li>
 
             )
         })
@@ -28,4 +29,7 @@ function mapStateToProps(state){
     }
 }
 
-export default connect(mapStateToProps)(UserList);
+function matchDispatchToProps(dispatch){
+    return bindActionCreators({selectUser: selectUser}, dispatch)
+}
+export default connect(mapStateToProps, matchDispatchToProps)(UserList);
